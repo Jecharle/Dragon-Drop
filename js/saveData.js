3f6848b735c1fa50eb632434b51eda961f7cc36c
Doktor Q <jecharle@mtu.edu>
2021-04-26 10:56:59 -0500

    Added real save / load options to saveData
    They aren't called yet, but it stores data in localStorage.

--- js/saveData.js
+++ js/saveData.js
@@ -6,12 +6,30 @@ class SaveData {
 	static mapEvent = {};
 	static options = {};
 
+	//#region mapEvent
 	static setEventClear(eventId) {
 		if (!eventId) return;
-		this.mapEvent[eventId] = true;
+		this.mapEvent[eventId] = 1;
 	}
 	static getEventClear(eventId) {
 		if (!eventId) return false;
 		return !!this.mapEvent[eventId];
 	}
+	//#endregion mapEvent
+
+	//#region save / load
+	static _save(attribute) {
+		var textData = JSON.stringify(this[attribute]);
+		localStorage.setItem(attribute, textData);
+	}
+	static _load(attribute) {
+		var textData = localStorage.getItem(attribute);
+		if (textData) {
+			this[attribute] = JSON.parse(textData);
+		}
+	}
+	static _delete(attribute) {
+		localStorage.removeItem(attribute);
+	}
+	//#endregion save / load
 }
