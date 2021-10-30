0d53b27e84eee0c918b8d21116d62a9dc2ad569b
Doktor Q <jecharle@mtu.edu>
2021-10-30 12:39:20 -0500

    Added sounds for basic combat effects
    Normal damage, critical damage, blocked damage, healing, and dying

--- js/piece.js
+++ js/piece.js
@@ -238,11 +238,11 @@ class UnitPiece extends Piece {
 	//#region sounds
 	_getSounds() {
 		// TODO: Get distinct sfx for these
-		this._sfxCritical = Sfx.getSfx("step1.wav");
-		this._sfxDamage = Sfx.getSfx("step1.wav");
-		this._sfxBlock = Sfx.getSfx("step1.wav");
-		this._sfxHeal = Sfx.getSfx("step1.wav");
-		this._sfxDie = Sfx.getSfx("step1.wav");
+		this._sfxCritical = Sfx.getSfx("critical_damage.wav");
+		this._sfxDamage = Sfx.getSfx("damage.wav");
+		this._sfxBlock = Sfx.getSfx("no_damage.wav");
+		this._sfxHeal = Sfx.getSfx("heal.wav");
+		this._sfxDie = Sfx.getSfx("death.wav");
 	}
 	//#endregion sounds
 
